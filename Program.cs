d11bdd20d0c366e07459675052f55f7191651bbb
ayun3738 <ayun3738@gmail.com>
2025-02-19 01:20:43 +0900

    250218
    dd

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿class Tmp{
+    private static void Kan(int n)
+    {
+        if (n == 0)
+        {
+            Console.Write("-");
+            return;
+        }
+        Kan(n - 1);
+        Console.Write(Enumerable.Repeat(" ", (int)Math.Pow(3, n - 1)).Aggregate((a, b) => a + b));
+        Kan(n - 1);
+    }
+
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            string? N = Console.ReadLine();
+            if (N == null)
+            {
+                return;
+            }
+
+            Kan(int.Parse(N));
+            Console.Write("\n");
+        }
+    }
+}
+
+
