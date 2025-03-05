149b59d55cf1363a250c7fb4efdf2e583ac7a037
NoAYun <ayun3738@gmail.com>
2025-03-05 16:44:53 +0900

    250305
    ddddd

--- Program.cs
+++ Program.cs
@@ -1,17 +1,19 @@
-﻿using System.Collections;
-using System.ComponentModel;
-using System.Globalization;
-
-class Tmp{
+﻿class Tmp{
     private static StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
     private static StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
     private static Dictionary<string, HashSet<int>> tasks = new Dictionary<string, HashSet<int>>();
     
-    private static void remove_candidate(string set_type, int target_number)
+    private static void remove_candidate(string set_type, int[,] board, int target_number)
     {
-        if (tasks.ContainsKey(set_type))
+        tasks[set_type].Remove(target_number);
+        if (tasks[set_type].Count == 0)
         {
-            tasks[set_type].Remove(target_number);
+            tasks.Remove(set_type);
+            return;
+        }
+        if (tasks[set_type].Count == 1)
+        {
+            fill_board(board, set_type);
         }
     }
 
@@ -19,47 +21,44 @@ class Tmp{
     {
         int number = int.Parse(task.Substring(1));
         int target_number = tasks[task].First();
-        // sw.WriteLine(task + " " + target_number);
-            int real_row;
-            int real_col;
-            for (int i = 0; i < 9; i++){
-                switch(task[0])
-                {
-                    case 'r':
-                        real_row = number;
-                        real_col = i;
-                        break;
-                    case 'c':
-                        real_row = i;
-                        real_col = number;
-                        break;
-                    default:
-                        real_row = number / 3 * 3 + i / 3;
-                        real_col = number % 3 * 3 + i % 3;
-                        break;
-                }    
-                if (board[real_row, real_col] == 0)
-                {
-                    board[real_row, real_col] = target_number;
-                    string row = 'r' + real_row.ToString();
-                    string col = 'c' + real_col.ToString();
-                    string box = 'b' + (real_row / 3 * 3 + real_col / 3).ToString();
-
-                }
-            }
-        sw.WriteLine(task + " " + target_number);
-        tasks[task].Remove(target_number);
-        if (tasks[task].Count == 1)
-        {
-            fill_board(board, task);
-        }
-        else if (tasks[task].Count == 0)
-        {
-            foreach (string key in tasks.Keys)
+        int real_row;
+        int real_col;
+        for (int i = 0; i < 9; i++){
+            switch(task[0])
             {
-                // Console.WriteLine(key);
+                case 'r':
+                    real_row = number;
+                    real_col = i;
+                    break;
+                case 'c':
+                    real_row = i;
+                    real_col = number;
+                    break;
+                default:
+                    real_row = number / 3 * 3 + i / 3;
+                    real_col = number % 3 * 3 + i % 3;
+                    break;
+            }    
+            if (board[real_row, real_col] == 0)
+            {
+                board[real_row, real_col] = target_number;
+                string row = 'r' + real_row.ToString();
+                string col = 'c' + real_col.ToString();
+                string box = 'b' + (real_row / 3 * 3 + real_col / 3).ToString();
+                if (tasks.ContainsKey(row))
+                {
+                    remove_candidate(row, board, target_number);
+                }
+                if (tasks.ContainsKey(col))
+                {
+                    remove_candidate(col, board, target_number);
+                }
+                if (tasks.ContainsKey(box))
+                {
+                    remove_candidate(box, board, target_number);
+                }
+                return ;
             }
-            tasks.Remove(task);
         }
     }
 
@@ -96,22 +95,32 @@ class Tmp{
                 }
             }
         }
-        int previous_len = 1;
-        sw.WriteLine();
+        
+        
         while(previous_len > 0)
         {
-            int current_len = tasks.Count;
-            if (current_len == previous_len)
+            List<string> target_tasks = new List<string>();
+            foreach (string task in tasks.Keys)
             {
-                fill_board(board, tasks.Keys.First());
-            }
-            foreach(string task in tasks.Keys)
-            {
-                // sw.WriteLine(task + " " + tasks[task].Count);
                 if (tasks[task].Count == 1)
                 {
-                    fill_board(board, task);
+                    target_tasks.Add(task);
                 }
+            }
+            foreach(string task in target_tasks)
+            {
+                if (tasks.ContainsKey(task))
+                {
+                    if (tasks[task].Count == 1)
+                    {
+                        fill_board(board, task);
+                    }
+                }
+            }
+            int current_len = tasks.Count;
+            if (current_len == previous_len & current_len > 0)
+            {
+                fill_board(board, tasks.Keys.First());
             }
             previous_len = current_len;
         }
